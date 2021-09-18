91b45ff72fecd4d6c27e860315882e76efc94ed3
pinkflamey <mavis@pinkflamey.nl>
2021-09-18 02:35:22 +0200

    Exercise InVariablesWeTrust finished and added

---
+++ L2/PYTB1L2InVariablesWeTrust/invariableswetrust.py
@@ -0,0 +1,24 @@
+
+
+Character1 = chr(97)
+
+Float1 = 420.69
+
+Boolean1 = True
+Boolean2 = Boolean1
+
+Integer1 = 17
+Integer2 = 2004
+Integer3 = Integer1 - Integer2
+
+String1 = "Dit is zo een "
+String2 =  "volledige zin."
+String3 =  String1 + String2
+
+print("Character1: " + str(Character1))
+print("Float1: " + str(Float1))
+print("Boolean2: " + str(Boolean2))
+print("Integer3: " + str(Integer3))
+print("String3: " + str(String3))
+
+quit = input("Enter to quit...")
