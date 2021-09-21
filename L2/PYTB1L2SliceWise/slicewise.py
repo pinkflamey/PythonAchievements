819d5e3809b3e1ee1c8517e65b50bddfef5e9dde
pinkflamey <mavis@pinkflamey.nl>
2021-09-21 11:15:09 +0200

    finished exercise PYTB1L2SliceWise

---
+++ L2/PYTB1L2SliceWise/slicewise.py
@@ -0,0 +1,17 @@
+
+
+tekst = 'De Python lessen worden gegeven voor Erik, Erwin en ook door Hidde'
+voornamen = [tekst[37:41], tekst[43:48], tekst[61:67]]
+print(*voornamen, sep=" + ")
+
+tekst = "SD vakken zijn bijvoorbeeld Python, UXD, Frontend development en Backend development. Wat leuk!"
+vakken = [tekst[0:2], tekst[28:34], tekst[36:39], tekst[41:61], tekst[65:84]]
+print(*vakken, sep=" + ")
+
+tekst = "Wat is hier het laatste woord?"
+laatste_woord = [tekst[24:29]]
+print(*laatste_woord, sep=" + ")
+
+tekst = "Het www is ontwikkeld vanaf 1989 door Tim Berners-Lee"
+chars = [tekst[5:9], tekst[29:34]]
+print(*chars, sep=" + ")
