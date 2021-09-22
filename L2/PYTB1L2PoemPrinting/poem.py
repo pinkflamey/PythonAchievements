2b5da976103a5328cab397faeeea66d2acba9e6c
pinkflamey <mavis@pinkflamey.nl>
2021-09-22 09:03:08 +0200

    Added and finished PYTB1L2PoemPrinting

---
+++ L2/PYTB1L2PoemPrinting/poem.py
@@ -0,0 +1,10 @@
+vers1 = "Mijn naam is Mavis en ik woon in Sassenheim,\n"
+vers2 = "Al mijn hele leven en in principe is dat wel fijn?\n"
+vers3 = "Er is niet al te veel te doen,\n"
+vers4 = "Maar het is niet lelijk en er is best wat groen.\n"
+vers5 = "Ik ben te lui om meer zinnen te bedenken,\n"
+vers6 = "Dus is dit de laatste zin die ik dit gedicht zal schenken.\n\n"
+
+print(vers1 + vers2 + vers3 + vers4 + vers5 + vers6)
+
+input("Enter to quit...")
