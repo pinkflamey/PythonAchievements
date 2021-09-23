ff304e65d8268761e4c38996edf11e0d1f9057b1
pinkflamey <mavis@pinkflamey.nl>
2021-09-23 09:33:39 +0200

    Added and finished exercise PYTB1L2StutterSimulator

---
+++ L2/PYTB1L2StutterSimulator/stuttersim.py
@@ -0,0 +1,16 @@
+from random import randint
+
+
+string = input("Insert string: ")
+
+strlist = string.split()
+
+for word in strlist:
+    ran1 = randint(1, 4)
+    
+    stutword1 = word[0:ran1] 
+    
+    if stutword1 != word:
+        print(stutword1[0:1] + "... " + stutword1 + "... " + word, end=" ")
+    else:
+        print (word, end=" ")
