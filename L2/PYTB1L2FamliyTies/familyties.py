5c3e4de0c4bcf325cdcd8fd4bb92a7b78281e91e
pinkflamey <mavis@pinkflamey.nl>
2021-09-23 09:40:05 +0200

    Fixed small error in PYTB1L2FamliyTies

--- L2/PYTB1L2FamliyTies/familyties.py
+++ L2/PYTB1L2FamliyTies/familyties.py
@@ -19,6 +19,6 @@ print("Ik heet " + firstname + " " + surname + ".", end=" ")
 print("Ik ben " + str(age) + " jaar oud, en ik woon in " + town + ". Mijn adres is " + address + ". Mijn haarkleur is nu", end=" ")
 print(*hair_colour, sep=" & ", end="")
 print(".")
-print("Ik ga naar het " + school + " in " + school_town + ". Het adres is " + school_address + ", en de afstand naar school is " + str(distance_to_school) + ".")
+print("Ik ga naar het " + school + " in " + school_town + ". Het adres is " + school_address + ", en de afstand naar school is " + str(distance_to_school) + "km.")
 
 input("Press enter to quit...")
