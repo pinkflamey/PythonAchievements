b561c9e81bfe687230dab432d312f8264d85cd7f
pinkflamey <mavis@pinkflamey.nl>
2021-09-28 09:06:34 +0200

    Added (not finished) exercise PYTB1L4WheresWaldo

---
+++ L4/PYTB1L4WheresWaldo/whereswaldo.py
@@ -0,0 +1,6 @@
+import random
+
+
+people = ["Waldo","Jackson","Ray","Eric","Vaughn","Jesse","Herbert","Robert", "Rodrigo","Elija","Sasha","Nathaniel","Ellie","Allison","Jeremiah", "Paula", "Alisha","Tory","Troy", "Faye"]
+searchlist = random.shuffle(people)
+
